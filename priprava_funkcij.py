cb20f03f0a13d227e5ce47d087b3ee081ef32850
Matic Matušek <maticmatusek@gmail.com>
2022-11-06 15:31:43 +0100

    Dokončan brute force

--- priprava_funkcij.py
+++ priprava_funkcij.py
@@ -9,6 +9,13 @@ from sympy import symbols, solve
 from itertools import count, permutations 
 
 def naredi_trikotnik(dolzina=None,spodnja_meja=1,zgornja_meja=10,celostevilski=True):
+    """ 
+    Naredi trikotnik:
+    * Sprejme : dolžino, spodnjo/zgornjo mejo, celoštevilskost
+    * Naključen položaj noge
+    * Vrstni red je 0
+    Vrne : trikotnik
+    """
     if dolzina == None and celostevilski:
         dolzina = random.randint(spodnja_meja,zgornja_meja)
     if dolzina == None and celostevilski == False :
@@ -29,6 +36,9 @@ def naredi_trikotnik(dolzina=None,spodnja_meja=1,zgornja_meja=10,celostevilski=T
     return trikotnik
 
 def narisi_trikotnik(trikotnik):
+    """
+    Nariše trikotnik
+    """
     x = trikotnik["polozaj_noge"]
     x_a_do_b = np.array([trikotnik["a"][0](x), trikotnik["b"][0](x)])
     x_a_do_c = np.array([trikotnik["a"][0](x), trikotnik["c"][0](x)])
@@ -42,6 +52,11 @@ def narisi_trikotnik(trikotnik):
     return ab_stranica, bc_stranica, ac_stranica
 
 def naredi_trikotnike(stevilo_trikotnikov, dolzine = None,spodnja_meja=1,zgornja_meja=10,celostevilski=True):
+    """
+    * Naredi n trikotnikov (PAZI stevilo_trikotnikov mora biti enako kot dolzina seznama)
+    * vrstni red doloci glede na številko trikotnika 
+    * vrne slovar trikotnikov brez specifično določenih vrstnih redov in položaj nog
+    """
     slovar = dict()
     if dolzine == None:
         for i in range(stevilo_trikotnikov):
@@ -57,6 +72,9 @@ def naredi_trikotnike(stevilo_trikotnikov, dolzine = None,spodnja_meja=1,zgornja
     return slovar
 
 def narisi_trikotnike(trikotniki):
+    """
+    sprejme slovar trikotnikov in jih izriše
+    """
     konec = []
     dolzina = []
     dolzina_urnik = dolzina_urnika(trikotniki)
@@ -69,29 +87,41 @@ def narisi_trikotnike(trikotniki):
     plt.plot(dolzina_x,dolzina_y,"r:^")
     plt.plot(np.array([0,0]),np.array([0,max(dolzina)]),"r:")
     plt.plot(np.array([max(konec),max(konec)]),np.array([0,max(dolzina)]),"r:")
-    plt.text(x=max(konec)/2,y=max(dolzina)+1,s=f"Dolžina urnika: {dolzina_urnik}",color="r")
+    plt.text(x=max(konec)/2,y=max(dolzina)+3,s=f"Dolžina urnika: {dolzina_urnik}",color="r")
+    ax = plt.gca()
+    ax.set_aspect('equal', adjustable='box')
     return plt.show()
 
 def dolzina_urnika(trikotniki):
-    seznam_polozaj_noge = []
-    seznam_dolzina = []
+    """
+    Sprejme slovar trikotnikov in izračuna max(polozaj_noge + dolzina)
+    """
+    dolzine = []
     for i in range(len(trikotniki)):
-        seznam_dolzina.append(trikotniki[i]["dolzina"])
-        seznam_polozaj_noge.append(trikotniki[i]["polozaj_noge"])
-    maksimum = max(seznam_polozaj_noge)
-    for i in range(len(seznam_polozaj_noge)):
-        if maksimum == seznam_polozaj_noge[i]:
-            return seznam_polozaj_noge[i] + seznam_dolzina[i]
+        dolzine.append(trikotniki[i]["polozaj_noge"]+trikotniki[i]["dolzina"])
+    return max(dolzine)
+
     
-def razvrsti_v_seznam(trikotniki):
+def razvrsti_v_seznam_glede_na_vrstni_red(trikotniki):
+    """
+    * Pomožna funkcija sprejme seznam trikotnikov in jih razvrsti v seznam po vrsti glede na vrstni_red
+    * Vrne 2 seznama in sicer 1) seznam trikotnikov 2) seznam imen trikotnikov
+    * Funkcija se uporabi v: iz_slovarja_doloci_polozaj_noge_nazaj_v_slovar(trikotniki)
+    """
     seznam = [None] * len(trikotniki)
     seznam_imen = [None] * len(trikotniki)
     for i in range(len(trikotniki)):
         seznam[trikotniki[i]["vrstni_red"]] = trikotniki[i]
         seznam_imen[trikotniki[i]["vrstni_red"]] = i
-    return seznam
+    return seznam , seznam_imen
 
 def doloci_polozaj_noge_glede_na_vrstni_red(urejeni_trikotniki_v_seznamu):
+    """
+    * Pomožna funkcija
+    * spremeni zgolj položaj_noge
+    * sprejme seznam urejenih trikotnikov in določi položaj noge glede na vrstni red
+    * Funkcija se uporabi v: iz_slovarja_doloci_polozaj_noge_nazaj_v_slovar(trikotniki)
+    """
     urejeni_trikotniki_v_seznamu[0]["polozaj_noge"] = 0
     for i in range(1,len(urejeni_trikotniki_v_seznamu)):
         if urejeni_trikotniki_v_seznamu[i-1]["dolzina"] >= urejeni_trikotniki_v_seznamu[i]["dolzina"]:
@@ -104,25 +134,51 @@ def doloci_polozaj_noge_glede_na_vrstni_red(urejeni_trikotniki_v_seznamu):
                     seznam[j] = 0
                 elif urejeni_trikotniki_v_seznamu[j]["dolzina"] > urejeni_trikotniki_v_seznamu[i]["dolzina"]:
                     seznam[j] =  urejeni_trikotniki_v_seznamu[j]["dolzina"] -( (urejeni_trikotniki_v_seznamu[j]["dolzina"]) - (urejeni_trikotniki_v_seznamu[i]["dolzina"])  ) - (urejeni_trikotniki_v_seznamu[i]["polozaj_noge"] - urejeni_trikotniki_v_seznamu[j]["polozaj_noge"] )
-                elif urejeni_trikotniki_v_seznamu[j]["dolzina"] < urejeni_trikotniki_v_seznamu[i]["dolzina"]:
+                elif urejeni_trikotniki_v_seznamu[j]["dolzina"] <= urejeni_trikotniki_v_seznamu[i]["dolzina"]:
                     seznam[j] = urejeni_trikotniki_v_seznamu[i]["dolzina"] - ( urejeni_trikotniki_v_seznamu[i]["dolzina"] - urejeni_trikotniki_v_seznamu[j]["dolzina"] ) - (urejeni_trikotniki_v_seznamu[i]["polozaj_noge"] - urejeni_trikotniki_v_seznamu[j]["polozaj_noge"] )
             index = seznam.index(max(seznam))
             urejeni_trikotniki_v_seznamu[i]["polozaj_noge"] = urejeni_trikotniki_v_seznamu[i]["polozaj_noge"] + seznam[index]
     return urejeni_trikotniki_v_seznamu
 
-def iz_seznama_v_slovar(seznam):
+def iz_seznama_v_slovar(seznam, seznam_imen):
+    """
+    * Pomožna funkcija
+    * Sprejme seznam trikotnikov in seznam imen potem pa zapise trikotnike nazaj v slovar
+    * Funkcija se uporabi v: iz_slovarja_doloci_polozaj_noge_nazaj_v_slovar(trikotniki)
+    """
     trikotniki = dict()
-    for i in range(len(seznam)):
-        trikotniki[i]=seznam[i]
+    for i in range(len(seznam_imen)):
+        trikotniki[seznam_imen[i]] = seznam[i]
     return trikotniki
 
 
-#narisi_trikotnike( iz_seznama_v_slovar( doloci_polozaj_noge_glede_na_vrstni_red( razvrsti_v_seznam( naredi_trikotnike(6,[9,2,1,1.5,3,10]) ) ) ) )
-#narisi_trikotnike( iz_seznama_v_slovar( doloci_polozaj_noge_glede_na_vrstni_red( razvrsti_v_seznam( naredi_trikotnike(6,[9,2,0.9,2.1,3,10]) ) ) ) )
+#narisi_trikotnike( iz_seznama_v_slovar( doloci_polozaj_noge_glede_na_vrstni_red( razvrsti_v_seznam_glede_na_vrstni_red( naredi_trikotnike(6,[9,2,1,1.5,3,10]) ) ) ) )
+#narisi_trikotnike( iz_seznama_v_slovar( doloci_polozaj_noge_glede_na_vrstni_red( razvrsti_v_seznam_glede_na_vrstni_red( naredi_trikotnike(6,[9,2,0.9,2.1,3,10]) ) ) ) )
 
+def iz_slovarja_doloci_polozaj_noge_nazaj_v_slovar(trikotniki):
+    """
+    Funkcija, ki sprejme trikotnike katerim mora biti določen vrstni red, da lahko izračuna položaje nog
+    Vrne slovar trikotnikov
+    """
+    seznam, seznam_imen = razvrsti_v_seznam_glede_na_vrstni_red(trikotniki)
+    dolocen_polozaj_noge = doloci_polozaj_noge_glede_na_vrstni_red(seznam)
+    return iz_seznama_v_slovar(dolocen_polozaj_noge,seznam_imen)
+
+def apply_permutacijo_na_trikotnike(permutacija,trikotniki):
+    """
+    Pomožna funkcija, ki trikotnikom nastavi vrstni red glede na permutacijo
+    Permutacija: (x,y,z,...)
+    """
+    for i in range(len(trikotniki)):
+        trikotniki[i]["vrstni_red"] = permutacija[i]
+    return trikotniki
 
 # ni vredu brut fore bo treba še enkrat narest
 def brute_force(trikotniki):
+    """ 
+    * Vrne slovar optimalno postavljenih trikotnikov, ki imajo že določen položaj noge
+    * Potrebno zgolj še narisati
+    """
     trikotniki1 = trikotniki
     dolzina = []
     parmutacija = []
@@ -131,24 +187,20 @@ def brute_force(trikotniki):
         seznam.append(i)
     perm = permutations(seznam) 
     for i in list(perm): 
-        for j in trikotniki:
-            trikotniki1[j]["vrstni_red"] = i[j]
-        print(i)
-        print(trikotniki1)
+        trikotniki1 = apply_permutacijo_na_trikotnike(i,trikotniki1)
+        trikotniki1 = iz_slovarja_doloci_polozaj_noge_nazaj_v_slovar(trikotniki1)
         dolzina.append(dolzina_urnika(trikotniki1))
         parmutacija.append(i)
-        print(dolzina_urnika(trikotniki1))
-    print(dolzina)
     index = dolzina.index(min(dolzina))
     najboljsa_permutacija = parmutacija[index]
-    for k in trikotniki1:
-            trikotniki1[k]["vrstni_red"] = najboljsa_permutacija[k]
-    print(najboljsa_permutacija)
-    print(trikotniki1)
-    return trikotniki1
+    return iz_slovarja_doloci_polozaj_noge_nazaj_v_slovar(apply_permutacijo_na_trikotnike(najboljsa_permutacija,trikotniki1))
     
+
+
 #test = naredi_trikotnike(10,[1,2,1,10,1,1,1,20,4,1])
-#test =  naredi_trikotnike(3,[1,2,10])
-#narisi_trikotnike( iz_seznama_v_slovar( doloci_polozaj_noge_glede_na_vrstni_red( razvrsti_v_seznam( test ) ) ) )
-#brut = brute_force(test)
-#narisi_trikotnike( iz_seznama_v_slovar( doloci_polozaj_noge_glede_na_vrstni_red( razvrsti_v_seznam( brut ) ) ) )        
+test =  naredi_trikotnike(6)
+#narisi_trikotnike( iz_seznama_v_slovar( doloci_polozaj_noge_glede_na_vrstni_red( razvrsti_v_seznam_glede_na_vrstni_red( test ) ) ) )
+#narisi_trikotnike(iz_slovarja_doloci_polozaj_noge_nazaj_v_slovar(test))
+brut = brute_force(naredi_trikotnike(6))
+narisi_trikotnike(brute_force(naredi_trikotnike(8 , zgornja_meja=50)))
+#narisi_trikotnike( iz_seznama_v_slovar( doloci_polozaj_noge_glede_na_vrstni_red( razvrsti_v_seznam_glede_na_vrstni_red( brut ) ) ) )        
